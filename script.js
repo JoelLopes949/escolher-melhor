686e617db11c6f74f7c8a2c6b031d2cec531b83d
JoelLopes949 <joelyalopes2710@gmail.com>
2025-07-03 22:56:31 -0300

    football

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const jogadores = [
     { nome: "Luis Suárez", imagem: "imagens/luis-suarez.jpg" },
     { nome: "Ronaldinho Gaúcho", imagem: "imagens/ronaldinho-gaucho.jpg" },
     { nome: "Cristiano Ronaldo", imagem: "imagens/cr7.jpg" },
-    { nome: "Lionel Messi", imagem: "imagens/Lionel_Messi.JPG" },
+    { nome: "Lionel Messi", imagem: "imagens/Messi.jpg" },
     { nome: "Roberto Baggio", imagem: "imagens/roberto-baggio.jpg" },
     { nome: "Gerd Müller", imagem: "imagens/gerd-muller.jpg" },
     { nome: "Pele", imagem: "imagens/Pelé_1960.jpg" },
